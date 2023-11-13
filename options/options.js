4ae4f6980160462c0b1d2fc0206746ef94c1817e
Crowdbotics <team@crowdbotics.com>
2023-11-13 11:25:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calhal-44507",
+  url: "https://calhal-44507.botics.co",
+  api: "https://calhal-44507.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
